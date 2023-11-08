143f07948a9c2e8f9fd02703fa9290898377dbf2
kmc77 <107791142+kmc77@users.noreply.github.com>
2023-11-08 22:37:55 +0900

    Delete ITDA directory

--- ITDA/main/java/com/itda/ITDA/service/adMemberService.java
+++
@@ -1,7 +0,0 @@
-package com.itda.ITDA.service;
-
-public interface adMemberService {
-	public int isId(String id, String Pass);
-
-	public int isId(String id);
-}
--- ITDA/src/main/java/com/itda/ITDA/service/UserCategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.itda.ITDA.service;
-
-import com.itda.ITDA.domain.UserCategory;
-
-public interface UserCategoryService {
-
-	int insert(UserCategory userCategory);
-
-}
